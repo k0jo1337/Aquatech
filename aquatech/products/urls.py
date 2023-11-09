1a2d7525fc4be92003c6b30b4339fb6300caad84
k0jo1337 <k0jo1337@yandex.ru>
2023-11-09 23:32:28 +0500

    добавил страницу

--- aquatech/products/urls.py
+++ aquatech/products/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('products', views.products, name='products')
+    path('products', views.products, name='products'),
+    path('A60_WHITE', views.A60_WHITE, name='A60-WHITE')
 ]
 
-#san
--- aquatech/products/views.py
+++ aquatech/products/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def products(request):
     return render(request, 'products.html')
+
+
+def A60_WHITE(request):
+    return render(request, 'items/A60 WHITE.html')
+
+
