a939a36011c8720b8fbf551341d88df4287c29e5
k0jo1337 <k0jo1337@yandex.ru>
2023-12-03 21:34:10 +0500

    добавил страницу

--- aquatech/products/urls.py
+++ aquatech/products/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('products', views.products, name='products'),
-    path('A60_WHITE', views.A60_WHITE, name='A60-WHITE')
+    path('A60-WHITE', views.A60_WHITE, name='A60-WHITE')
 ]
 
