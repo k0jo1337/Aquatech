79a5a340f20e92f11f7e52a254c6711f8cb41bcf
siximapala <110502366+siximapala@users.noreply.github.com>
2023-12-06 15:16:56 +0500

    Добавил Т98 пурифайер

--- aquatech/products/urls.py
+++ aquatech/products/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('products', views.products, name='products'),
     path('products/A60-WHITE', views.A60_WHITE, name='A60-WHITE'),
-    path('products/A60-BLACK', views.A60_BLACK, name='A60-BLACK')
+    path('products/A60-BLACK', views.A60_BLACK, name='A60-BLACK'),
+    path('products/T98-WHITE', views.T98_WHITE, name='T98-WHITE')
 ]
 
--- aquatech/products/views.py
+++ aquatech/products/views.py
@@ -13,4 +13,5 @@ def A60_BLACK(request):
     return render(request, 'items/A60-BLACK.html')
 
 
-
+def T98_WHITE(request):
+    return render(request, 'items/T98-WHITE.html')
