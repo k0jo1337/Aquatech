621ea183db4021b015c3584cb74b1236e0d207da
siximapala <110502366+siximapala@users.noreply.github.com>
2023-12-15 02:36:15 +0500

    Сделал все страницы пурифайеров и активировал кнопки в /products

--- aquatech/products/urls.py
+++ aquatech/products/urls.py
@@ -6,6 +6,14 @@ urlpatterns = [
     path('products', views.products, name='products'),
     path('products/A60-WHITE', views.A60_WHITE, name='A60-WHITE'),
     path('products/A60-BLACK', views.A60_BLACK, name='A60-BLACK'),
-    path('products/T98-WHITE', views.T98_WHITE, name='T98-WHITE')
+    path('products/T98-WHITE', views.T98_WHITE, name='T98-WHITE'),
+    path('products/T98-BLACK', views.T98_BLACK, name='T98-BLACK'),
+    path('products/V11-WHITE', views.V11_WHITE, name='V11-WHITE'),
+    path('products/A62-WHITE', views.A62_WHITE, name='A62-WHITE'),
+    path('products/C21-U4LE-SILVER', views.C21_U4LE_SILVER, name='C21-U4LE-SILVER'),
+    path('products/A72-BLACKNWHITE', views.A72_BLACKNWHITE, name='A72-BLACKNWHITE'),
+    path('products/H1-U4LE-WHITENSILVER', views.H1_U4LE_WHITENSILVER, name='H1-U4LE-WHITENSILVER'),
+    path('products/H1-U4L-WHITE', views.H1_U4L_WHITE, name='H1-U4LE-WHITE'),
+    path('products/H40-U4L-BLACKNWHITE', views.H40_U4L_BLACKNWHITE, name='H40-U4L-BLACKNWHITE'),
 ]
 
--- aquatech/products/views.py
+++ aquatech/products/views.py
@@ -12,6 +12,29 @@ def A60_WHITE(request):
 def A60_BLACK(request):
     return render(request, 'items/A60-BLACK.html')
 
-
 def T98_WHITE(request):
     return render(request, 'items/T98-WHITE.html')
+
+def T98_BLACK(request):
+    return render(request, 'items/T98-BLACK.html')
+
+def V11_WHITE(request):
+    return render(request, 'items/V11-WHITE.html')
+
+def A62_WHITE(request):
+    return render(request, 'items/A62-WHITE.html')
+
+def C21_U4LE_SILVER(request):
+    return render(request, 'items/C21-U4LE-SILVER.html')
+
+def A72_BLACKNWHITE(request):
+    return render(request, 'items/A72-BLACKNWHITE.html')
+
+def H1_U4LE_WHITENSILVER(request):
+    return render(request, 'items/H1-U4LE-WHITENSILVER.html')
+
+def H1_U4L_WHITE(request):
+    return render(request, 'items/H1-U4L-WHITE.html')
+
+def H40_U4L_BLACKNWHITE(request):
+    return render(request, 'items/H40-U4L-BLACKNWHITE.html')
