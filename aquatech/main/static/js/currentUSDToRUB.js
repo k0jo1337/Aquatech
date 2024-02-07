bcbaad7a4ee51597bfd577d0bf8a93a0b2c79da3
siximapala <110502366+siximapala@users.noreply.github.com>
2024-02-07 17:37:23 +0500

    Сохранение цены товара локально, чтобы не запрашивать API каждый раз

--- aquatech/main/static/js/currentUSDToRUB.js
+++ aquatech/main/static/js/currentUSDToRUB.js
@@ -16,56 +16,75 @@ const H1_U4L_WHITE_USD_PRICE_CONSTANT = 295;
 
 const H40_USD_PRICE_CONSTANT = 228;
 
-const renderPriceContent = ( response ) =>{
-    const api_json_data = JSON.parse(JSON.stringify((response)));
-    dollarToRuble = api_json_data.data.response.value;
-    return dollarToRuble;
+const currentDate = new Date().toDateString(); //при каждом обновлении/заходе на страницу чекается дата
+
+
+function getNewDollarToRuble() { //функция получающая данные с API
+  return axios
+      .get("https://api.currencybeacon.com/v1/convert?from=USD&to=RUB&amount=1&api_key=P1fJMk4l8zde8s70h5PGIROCKu1k1Kx4")
+      .then((response) => {
+          const api_json_data = JSON.parse(JSON.stringify(response));
+          return api_json_data.data.response.value;
+      });
+}
+
+function setPriceByDocument(){ //функция для изображения цены на фронте
+  priceElement = document.getElementById('price');
+  switch (document.location.pathname){
+    case "/products/A60-BLACK":
+      priceElement.innerHTML = Math.floor(A60_BLACK_USD_PRICE_CONSTANT * localStorage.getItem('currencyExchangeRate'));
+      break;
+    case "/products/A60-WHITE":
+      priceElement.innerHTML = Math.floor(A60_WHITE_USD_PRICE_CONSTANT * localStorage.getItem('currencyExchangeRate'));
+      break;
+    case "/products/T98-WHITE":
+      priceElement.innerHTML = Math.floor(T98_WHITE_USD_PRICE_CONSTANT * localStorage.getItem('currencyExchangeRate'));
+      break;
+    case "/products/T98-BLACK":
+      priceElement.innerHTML = Math.floor(T98_BLACK_USD_PRICE_CONSTANT * localStorage.getItem('currencyExchangeRate'));
+      break;
+    case "/products/V11-WHITE":
+      priceElement.innerHTML = Math.floor(V11_WHITE_USD_PRICE_CONSTANT * localStorage.getItem('currencyExchangeRate'));
+      break;
+    case "/products/A62-WHITE":
+      priceElement.innerHTML = Math.floor(A62_WHITE_USD_PRICE_CONSTANT * localStorage.getItem('currencyExchangeRate'));
+      break;
+    case "/products/C21-U4LE-SILVER":
+      priceElement.innerHTML = Math.floor(C21_BLACK_USD_PRICE_CONSTANT * localStorage.getItem('currencyExchangeRate'));
+      break;
+    case "/products/A72-BLACKNWHITE":
+      priceElement.innerHTML = Math.floor(A72_BLACK_USD_PRICE_CONSTANT * localStorage.getItem('currencyExchangeRate'));
+      break;
+    case "/products/H1-U4LE-WHITENSILVER":
+      priceElement.innerHTML = Math.floor(H1_U4LE_WHITENSILVER_USD_PRICE_CONSTANT * localStorage.getItem('currencyExchangeRate'));
+      break;
+    case "/products/H1-U4L-WHITE":
+      priceElement.innerHTML = Math.floor(H1_U4L_WHITE_USD_PRICE_CONSTANT * localStorage.getItem('currencyExchangeRate'));
+      break;
+    case "/products/H40-U4L-BLACKNWHITE":
+      priceElement.innerHTML = Math.floor(H40_USD_PRICE_CONSTANT * localStorage.getItem('currencyExchangeRate'));  
+      break;
+    };
 }
 
 
-function getNewDollarToRuble(){
-axios
-    .get("https://api.currencybeacon.com/v1/convert?from=USD&to=RUB&amount=1&api_key=P1fJMk4l8zde8s70h5PGIROCKu1k1Kx4")
-    .then((response) => {
-        dollarToRuble = renderPriceContent(response);
-        return dollarToRuble;
-    })
+/* У нас мало токенов. Поэтому после того как пользователь впервые делает API запрос,
+   значение курса сохраняется в localStorage. Если localStorage был сохранён в другой день (APIшка
+   обновляет курс раз в стуки), а не в тот, в который зашли на сайт, то он снова делает API
+   запрос и сохраняет курс. */
+if (!localStorage.getItem('lastDV') || localStorage.getItem('lastDV') !== currentDate){
+
+  if (!localStorage.getItem('lastDV')){console.log('LOCAL_FILE_NOT_FOUND')} //
+  else {console.log('NEW DAY!!!')};                                         //Для дебага, удалить
+
+  getNewDollarToRuble().then((dollarToRuble) => {
+    localStorage.setItem('currencyExchangeRate', dollarToRuble.toString());
+    setPriceByDocument();
+  });
+  localStorage.setItem('lastDV', currentDate);
+}
+else{
+  window.onload = function(){
+    setPriceByDocument();
   }
-
-dollarToRuble = getNewDollarToRuble();
-var cache = require('memory-cache');
-cache.put("dollarToRubleToday",dollarToRuble, 1000 * 60 * 60 * 24, getNewDollarToRuble)
-
-priceElement = document.getElementById('price');
-switch (document.location.pathname){
-case "/products/A60-BLACK":
-  priceElement.innerHTML = Math.floor(A60_BLACK_USD_PRICE_CONSTANT * dollarToRuble);
-  break;
-case "/products/A60-WHITE":
-  priceElement.innerHTML = Math.floor(A60_WHITE_USD_PRICE_CONSTANT * dollarToRuble);
-  break;
-case "/products/T98-WHITE":
-  priceElement.innerHTML = Math.floor(T98_WHITE_USD_PRICE_CONSTANT * dollarToRuble);
-  break;
-case "/products/T98-BLACK":
-  priceElement.innerHTML = Math.floor(T98_BLACK_USD_PRICE_CONSTANT * dollarToRuble);
-  break;
-case "/products/V11-WHITE":
-  priceElement.innerHTML = Math.floor(V11_WHITE_USD_PRICE_CONSTANT * dollarToRuble);
-  break;
-case "/products/C21-U4LE-SILVER":
-  priceElement.innerHTML = Math.floor(C21_BLACK_USD_PRICE_CONSTANT * dollarToRuble);
-  break;
-case "/products/A72-BLACKNWHITE":
-  priceElement.innerHTML = Math.floor(A72_BLACK_USD_PRICE_CONSTANT * dollarToRuble);
-  break;
-case "/products/H1-U4LE-WHITENSILVER":
-  priceElement.innerHTML = Math.floor(H1_U4LE_WHITENSILVER_USD_PRICE_CONSTANT * dollarToRuble);
-  break;
-case "/products/H1-U4L-WHITE":
-  priceElement.innerHTML = Math.floor(H1_U4L_WHITE_USD_PRICE_CONSTANT * dollarToRuble);
-  break;
-case "/products/H40-U4L-BLACKNWHITE":
-  priceElement.innerHTML = Math.floor(H40_U4L_WHITE_USD_PRICE_CONSTANT * dollarToRuble);  
-  break;
 };
