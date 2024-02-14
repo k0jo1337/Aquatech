dc7a3e95c646beaf9c9a8a0055ecabe86775aa54
k0jo1337 <k0jo1337@yandex.ru>
2024-02-14 22:44:11 +0500

    Окно "связаться с нами"

--- aquatech/aquatech/settings.py
+++ aquatech/aquatech/settings.py
@@ -120,8 +120,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, STATIC_URL)]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sashas42748@gmail.com'
+EMAIL_HOST_PASSWORD = 'bjajganjfukzhpnw'
+
+
--- aquatech/main/views.py
+++ aquatech/main/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 def index(request):
     return render(request, 'main_page/main.html')
+
+
+class AppealTemplateView(TemplateView):
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get('email')
+        user_name = request.POST.get('user_name')
+        phone = request.POST.get('phone')
+
+        message = render_to_string('main/email/requests_email_send.html', {
+            'email': email,
+            'user_name': user_name,
+            'phone': phone,
+        })
+
+        send = EmailMessage(subject=user_name, body=message, from_email='sashas42748@gmail.com',
+                            to=['sashas42748@gmail.com'])
+        send.send(fail_silently=False)
+
+        return render(request, 'main_page/main.html')
+
+    def get(self, request, *args, **kwargs):
+        url = kwargs.get('url')
+        return self.render_to_response(self.get_context_data(url=url))
+
+
+
