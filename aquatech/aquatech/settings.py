e29e0a6ab38c7c50e7f7e66bac452dbd83450285
k0jo1337 <k0jo1337@yandex.ru>
2024-02-14 22:44:11 +0500

    Окно "связаться с нами"

--- aquatech/aquatech/settings.py
+++ aquatech/aquatech/settings.py
@@ -118,9 +118,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = 'main/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, STATIC_URL)]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sashas42748@gmail.com'
+EMAIL_HOST_PASSWORD = 'bjajganjfukzhpnw'
+
+
--- aquatech/main/urls.py
+++ aquatech/main/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
-
+from .views import AppealTemplateView
 
 urlpatterns = [
-    path('', views.index, name='homepage')
+    path('', views.index, name='homepage'),
+    # Add a catch-all pattern to capture any URL
+    re_path(r'^(?P<url>.*)/$', AppealTemplateView.as_view(), name='appeal'),
 ]
