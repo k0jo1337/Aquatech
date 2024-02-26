846e6e77c341b5a27f8089260749abaa5cedd6b6
siximapala <110502366+siximapala@users.noreply.github.com>
2024-02-26 17:21:59 +0500

    Добавил фавиконки, описани, и чуть-чуть почистил

--- aquatech/aquatech/settings.py
+++ aquatech/aquatech/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, STATIC_URL)]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
