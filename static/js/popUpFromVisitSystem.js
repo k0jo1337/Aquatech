7036f731680b78a29ce0fd42d596e40598df903b
siximapala <110502366+siximapala@users.noreply.github.com>
2024-03-26 15:19:49 +0500

    Добавил поп-ап уведомление о кнопке "Связаться с нами"

---
+++ static/js/popUpFromVisitSystem.js
@@ -0,0 +1,35 @@
+function enablePopUp(){
+    var popup = document.getElementById("myPopup");
+    popup.classList.add("show");
+
+    setTimeout(function() {
+        popup.classList.remove("show");
+    }, 20000);
+
+    document.getElementById("myPopup").addEventListener("click", function() {
+        removePopUp();
+        }, { once: true });
+
+    document.getElementById("menuToggle_main").addEventListener("click", function() {
+        removePopUp();
+    }, { once: true });
+}
+
+function removePopUp(){
+    var popup = document.getElementById("myPopup");
+    popup.classList.add("hide");
+    popup.addEventListener("animationend", function() {
+        popup.remove();
+    }, { once: true });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    if (localStorage.getItem('lastDV')) { //проверяется и сеттится в currentUSDToRUB
+        if (Math.random() <= 0.1) enablePopUp();
+    }
+     else { 
+        enablePopUp();
+     }
+
+    });
